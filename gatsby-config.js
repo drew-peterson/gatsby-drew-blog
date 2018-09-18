a46e70a8a15bbe4f256209f9687302bbac5d9fdd
weeznog <drew.m.peterson@gmail.com>
2018-09-18 15:17:17 -0700

    update path prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     title: 'Drew Gatsby Testing',
     desc: 'a new blog', // restart server to get changes in graphql
   },
-  pathPrefix: '/gatsby',
+  pathPrefix: '/gatsby-drew-blog',
   plugins: [
     'gatsby-plugin-react-helmet',
     {
